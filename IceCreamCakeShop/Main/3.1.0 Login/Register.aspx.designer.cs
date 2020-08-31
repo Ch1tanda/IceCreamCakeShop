598d21d30400d162d26b0dcb36de701f68224cba
HelpChi <69040286+HelpChi@users.noreply.github.com>
2020-08-31 18:45:15 +0800

    用户注册，识别闰年闰月
    千年女装帝国upup

--- IceCreamCakeShop/Main/3.1.0 Login/Register.aspx.designer.cs
+++ IceCreamCakeShop/Main/3.1.0 Login/Register.aspx.designer.cs
@@ -7,13 +7,11 @@
 // </自动生成>
 //------------------------------------------------------------------------------
 
-namespace IceCreamCakeShop.Main._3._1._0_Login
-{
-
-
-    public partial class Register
-    {
-
+namespace IceCreamCakeShop.Main._3._1._0_Login {
+    
+    
+    public partial class Register {
+        
         /// <summary>
         /// Label1 控件。
         /// </summary>
@@ -22,7 +20,7 @@ namespace IceCreamCakeShop.Main._3._1._0_Login
         /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
         /// </remarks>
         protected global::System.Web.UI.WebControls.Label Label1;
-
+        
         /// <summary>
         /// Label2 控件。
         /// </summary>
@@ -31,16 +29,16 @@ namespace IceCreamCakeShop.Main._3._1._0_Login
         /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
         /// </remarks>
         protected global::System.Web.UI.WebControls.Label Label2;
-
+        
         /// <summary>
-        /// TextBox1 控件。
+        /// name 控件。
         /// </summary>
         /// <remarks>
         /// 自动生成的字段。
         /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
         /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox TextBox1;
-
+        protected global::System.Web.UI.WebControls.TextBox name;
+        
         /// <summary>
         /// Label3 控件。
         /// </summary>
@@ -49,16 +47,16 @@ namespace IceCreamCakeShop.Main._3._1._0_Login
         /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
         /// </remarks>
         protected global::System.Web.UI.WebControls.Label Label3;
-
+        
         /// <summary>
-        /// TextBox2 控件。
+        /// password 控件。
         /// </summary>
         /// <remarks>
         /// 自动生成的字段。
         /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
         /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox TextBox2;
-
+        protected global::System.Web.UI.WebControls.TextBox password;
+        
         /// <summary>
         /// Label4 控件。
         /// </summary>
@@ -67,16 +65,16 @@ namespace IceCreamCakeShop.Main._3._1._0_Login
         /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
         /// </remarks>
         protected global::System.Web.UI.WebControls.Label Label4;
-
+        
         /// <summary>
-        /// TextBox3 控件。
+        /// repassword 控件。
         /// </summary>
         /// <remarks>
         /// 自动生成的字段。
         /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
         /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox TextBox3;
-
+        protected global::System.Web.UI.WebControls.TextBox repassword;
+        
         /// <summary>
         /// Label5 控件。
         /// </summary>
@@ -85,16 +83,16 @@ namespace IceCreamCakeShop.Main._3._1._0_Login
         /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
         /// </remarks>
         protected global::System.Web.UI.WebControls.Label Label5;
-
+        
         /// <summary>
-        /// TextBox4 控件。
+        /// phonenumber 控件。
         /// </summary>
         /// <remarks>
         /// 自动生成的字段。
         /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
         /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox TextBox4;
-
+        protected global::System.Web.UI.WebControls.TextBox phonenumber;
+        
         /// <summary>
         /// Label6 控件。
         /// </summary>
@@ -103,16 +101,16 @@ namespace IceCreamCakeShop.Main._3._1._0_Login
         /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
         /// </remarks>
         protected global::System.Web.UI.WebControls.Label Label6;
-
+        
         /// <summary>
-        /// TextBox5 控件。
+        /// address 控件。
         /// </summary>
         /// <remarks>
         /// 自动生成的字段。
         /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
         /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox TextBox5;
-
+        protected global::System.Web.UI.WebControls.TextBox address;
+        
         /// <summary>
         /// Label7 控件。
         /// </summary>
@@ -121,16 +119,16 @@ namespace IceCreamCakeShop.Main._3._1._0_Login
         /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
         /// </remarks>
         protected global::System.Web.UI.WebControls.Label Label7;
-
+        
         /// <summary>
-        /// DropDownList1 控件。
+        /// year 控件。
         /// </summary>
         /// <remarks>
         /// 自动生成的字段。
         /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
         /// </remarks>
-        protected global::System.Web.UI.WebControls.DropDownList DropDownList1;
-
+        protected global::System.Web.UI.WebControls.TextBox year;
+        
         /// <summary>
         /// Label8 控件。
         /// </summary>
@@ -139,16 +137,16 @@ namespace IceCreamCakeShop.Main._3._1._0_Login
         /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
         /// </remarks>
         protected global::System.Web.UI.WebControls.Label Label8;
-
+        
         /// <summary>
-        /// DropDownList2 控件。
+        /// month 控件。
         /// </summary>
         /// <remarks>
         /// 自动生成的字段。
         /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
         /// </remarks>
-        protected global::System.Web.UI.WebControls.DropDownList DropDownList2;
-
+        protected global::System.Web.UI.WebControls.TextBox month;
+        
         /// <summary>
         /// Label9 控件。
         /// </summary>
@@ -157,16 +155,16 @@ namespace IceCreamCakeShop.Main._3._1._0_Login
         /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
         /// </remarks>
         protected global::System.Web.UI.WebControls.Label Label9;
-
+        
         /// <summary>
-        /// DropDownList3 控件。
+        /// days 控件。
         /// </summary>
         /// <remarks>
         /// 自动生成的字段。
         /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
         /// </remarks>
-        protected global::System.Web.UI.WebControls.DropDownList DropDownList3;
-
+        protected global::System.Web.UI.WebControls.TextBox days;
+        
         /// <summary>
         /// Label10 控件。
         /// </summary>
@@ -175,7 +173,16 @@ namespace IceCreamCakeShop.Main._3._1._0_Login
         /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
         /// </remarks>
         protected global::System.Web.UI.WebControls.Label Label10;
-
+        
+        /// <summary>
+        /// ErrorTip 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label ErrorTip;
+        
         /// <summary>
         /// Button1 控件。
         /// </summary>
